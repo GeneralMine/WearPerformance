c98144076976ffbecc58d0ca6adfe2e8f24282b2
Haokun Zheng <zhenghk@yahoo.de>
2021-11-21 10:11:53 +0100

    Implemented item overview page

--- app/src/routes/cloth/[clothId].svelte
+++ app/src/routes/cloth/[clothId].svelte
@@ -4,29 +4,40 @@
 
 <script>
 	import ProgressBar from '$lib/Common/ProgressBar.svelte';
-	import { page } from '$app/stores';
+	import { navigating, page } from '$app/stores';
 	/* Database */
 	import { readObject, writeObject } from '$lib/localstorage';
 	let db = readObject('db');
-
-	$: cloth = db ? db.clothes.filter((el) => el.id === $page.params.clothId) : null;
+	import Log from '../log.svelte';
+import { destroy_block } from 'svelte/internal';
+	let cloth = db.clothes[$page.params.clothId];
+	//$: cloth = db ? db.clothes.filter((el) => el.id === $page.params.clothId) : null;
 </script>
 
 <svelte:head>
 	<title>Item Info | WearPerformance</title>
 </svelte:head>
 
-{#if db}
+{#if db && cloth}
 	<div class="clothPage">
 		<div class="clothListItemImg">
 			<img src={cloth.img} alt="" />
+			<h1> {cloth.name} </h1>
+			<div id="wrapper">
+				<div id="first">Number of times Worn: {cloth.wornCounter}</div>
+				<div id="second">Added: {cloth.daysAdded} days ago</div>
+			</div>
 		</div>
+		<div class="stats">
+		<h3>Your item footprint</h3>
 		<ProgressBar min="0" max="100" value="50" />
+		<br>
 		<p>Material: {cloth.material}</p>
+		<ProgressBar min="0" max="100" value="50" />
+		<br>
 		<p>Origin: {cloth.origin}</p>
-		<p>Number of times Worn: {cloth.wornCounter}</p>
-
-		<div />
+		<ProgressBar min="0" max="100" value="50" />
+		</div>
 	</div>
 {/if}
 
@@ -34,11 +45,41 @@
 	.clothPage {
 		width: 100%;
 		height: 100%;
+		text-align: center;
 	}
 
 	.clothListItemImg {
-		width: 50%;
-		margin: auto;
+		display: block;
+  		margin: auto;
 		padding: 0.3rem;
+		text-align: center;
+		width: 50%;
+	}
+
+	img {
+		width:100%;
+		height: auto;
+		border-radius: 20px;
+	}
+
+	.stats {
+		display: block;
+  		margin: auto;
+		padding: 0.3rem;
+		text-align: center;
+		width: 50%;
+	}
+
+	#wrapper {
+		overflow: hidden;
+    	width: 100%;
+	}
+	#first {
+		float:left;
+	    width: 50%;
+	}
+	#second {
+		float:left;
+		width: 50%;
 	}
 </style>
